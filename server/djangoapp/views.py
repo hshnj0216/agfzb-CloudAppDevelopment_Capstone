56ceddd51293ea300c7ea0c218407858559df7b7
hshnj0216 <ldecena1116@gmail.com>
2023-03-07 12:19:38 -0500

    saved progress

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -27,12 +27,27 @@ def contact(request):
     return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
-
+def login_request(request):
+    context = {}
+    #Check if the request method is post
+    if request.method == "POST":
+        #Get the username and password from the request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        #If user is valid, call the login method and pass the user
+        if user is not None:
+            login(request, user)
+            return redirect(request, 'djangoapp/home.html')
+        #If not, return to the login page
+        else:
+            return render(request, 'djangoapp/home.html', context)
+    else:
+        return render(request, 'djangoapp/home.html', context)
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect(request, 'djangoapp/home.html')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
