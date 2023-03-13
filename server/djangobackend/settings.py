d5583bac3c8a1c62e83cd9204a3dcccbc986a992
hshnj0216 <ldecena1116@gmail.com>
2023-03-13 11:36:02 -0400

    update models and admin

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ["localhost"]
 # Application definition
 
 INSTALLED_APPS = [
-    'djangoapp.apps.DjangoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoapp',
 ]
 
 MIDDLEWARE = [
